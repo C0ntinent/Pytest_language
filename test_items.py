ebcc0fe4138a27d89dcd108ad12232b1a3140ebd
Dmitry M <a1a1a123@mail.ru>
2022-08-11 23:49:03 +0500

    changed time of time.sleep

--- test_items.py
+++ test_items.py
@@ -7,6 +7,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_language_of_browser(browser):
     browser.get(link)
-    time.sleep(2)
+    time.sleep(30)
     basket = browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")
     assert basket, 'Нет кнопки'
